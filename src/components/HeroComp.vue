1b4b6f1894fce0ba679323264775aced9cd6bcb2
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 09:28:33 +0100

    Hero split in 2, add title and text on the left, image on the right

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -6,12 +6,35 @@ export default {
 
         }
     },
+    methods: {
+
+    },
 }
 
 </script>
 
 <template>
-    <section id="hero">hero</section>
+    <section id="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h1>A complete<br>developer toolset</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae sequi nesciunt omnis impedit nemo.
+                        Ut, eveniet asperiores dicta architecto minima et. Officiis quasi magnam nemo pariatur. Minus
+                        incidunt rem enim.</p>
+                    <button class="btn btn-primary">Get started</button>
+                    <button class="btn btn-outline-primary">Documentation</button>
+
+                </div>
+                <div class="col-6">
+                    <div class="about-img">
+                        <img src="../assets/img/about.png" alt="about image">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 </template>
 
@@ -19,4 +42,10 @@ export default {
 @use '../styles/general.scss' as *;
 @use '../styles/partials/variables' as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+h1 {
+    text-transform: capitalize;
+}
+
+.hero {}
 </style>

52bfc22e6115b92d84e535ce1fce1f025d49c5a0
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 13:06:30 +0100

    Move to a folder PricingPlans and import PriceCard

---
+++ src/components/Main/PricingPlansFolder/PricingPlans.vue
@@ -0,0 +1,40 @@
+<script >
+import PriceCard from './PriceCard.vue';
+
+export default {
+    name: 'PricingPlans',
+    components: {
+        PriceCard,
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <section id="pricing-plans">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-12">
+                    <h2>Our rates for developerrom Landrickeam</h2>
+                    <p>Start working with <span>Landrick</span> that can provide everything you need to generate<br>
+                        awareness, drive traffic,connect.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <PriceCard />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../styles/general.scss' as *;
+@use '../../../styles/partials/variables' as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
+</style>

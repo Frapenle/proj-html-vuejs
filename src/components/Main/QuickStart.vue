3e430acc63ee9d5c8f2968beec49ee9e9cc6c6e4
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 12:06:59 +0100

    Add dev tools info

--- src/components/Main/QuickStart.vue
+++ src/components/Main/QuickStart.vue
@@ -1,11 +1,11 @@
 <script >
-import PricingPlans from './PricingPlans.vue';
 
 export default {
     name: "QuickStart",
-    components: { PricingPlans },
     data() {
-        return {};
+        return {
+            devToolsList: ['npm', 'nuget', 'spm', 'github']
+        };
     },
 }
 
@@ -22,9 +22,20 @@ export default {
                         awareness, drive traffic,connect.</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <PricingPlans />
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <div class="box rounded-1">
+                        <ul class="d-flex text-center rounded-1">
+                            <li class="rounded-1" v-for="tool in devToolsList">
+                                {{ tool }}
+                            </li>
+                        </ul>
+                        <div class="text-tip d-flex gap-2">
+                            <p><span>&dollar;</span> npm install</p>
+                            <p><span>-g</span> claps.js</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -35,4 +46,37 @@ export default {
 @use '../../styles/general.scss' as *;
 @use '../../styles/partials/variables' as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+.box {
+    width: 550px;
+    height: 100px;
+    background-color: $bg-dev-tools;
+    padding: 20px;
+}
+
+ul {
+    border: 1px solid $border-dev-tool;
+    background-color: $dev-tool;
+    padding: 0;
+}
+
+li {
+    width: 100%;
+    color: $bg-white;
+    padding: .3rem 0;
+    cursor: pointer;
+
+    &:first-child {
+        background-color: $bg-dev-tools;
+    }
+}
+
+.text-tip {
+    font-size: .9rem;
+    color: $grey-text;
+
+    span {
+        color: $green-symbol;
+    }
+}
 </style>

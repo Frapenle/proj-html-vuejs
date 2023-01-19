faa670f3c8d8a68185a04174796a2da18b7fe60d
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 10:17:15 +0100

    Add key feature section

---
+++ src/components/Main/KeyFeature/FeaturesCard.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div class="card">
+        <div class="ico">
+            <img src="" alt="">
+        </div>
+        <h6>modular</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, sequi.</p>
+        <p><a href="#">Read more</a></p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.card {
+    padding: 1rem;
+}
+</style>
---
+++ src/components/Main/KeyFeature/KeyFeatures.vue
@@ -0,0 +1,45 @@
+<script >
+import FeaturesCard from "./FeaturesCard.vue";
+export default {
+    name: 'KeyFeatures',
+    components: {
+        FeaturesCard,
+    },
+
+    data() {
+        return {
+
+        }
+    },
+}
+
+//import
+</script>
+
+<template>
+    <section id="key-features">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Key Features</h2>
+                    <p>Start working with <span>Landrick</span> that can provide everything you need to generate<br>
+                        awareness, drive traffic,connect.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <div class="cards">
+                        <FeaturesCard />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../../styles/partials/variables' as *;
+@use '../../../styles/general.scss' as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
+</style>

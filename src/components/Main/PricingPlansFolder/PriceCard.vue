ce4eb8ec924dbf85e9291bf04f5a1fa114cf2109
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 13:07:08 +0100

    Add PriceCard component

---
+++ src/components/Main/PricingPlansFolder/PriceCard.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        <h1></h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'PriceCard',
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+<style lang="scss">
+
+</style>

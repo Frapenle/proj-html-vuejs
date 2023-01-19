98458e6f80815e1c480fbc0a854f6916c4932e14
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 17:05:14 +0100

    Add props

--- src/components/Main/PricingPlansFolder/PriceCard.vue
+++ src/components/Main/PricingPlansFolder/PriceCard.vue
@@ -1,32 +1,46 @@
 <template lang="">
-    <div>
-        <div class="plan-card rounded-2">
-            <div class="sticker rounded-5 d-inline-block">Basic</div>
-            <div class="price">&dollar; 9
-                <br>
-                <span>Per month</span>
+    <!-- <div class="row"> -->
+        <div class="col-4">
+            <div class="plan-card rounded-2">
+                <div class="plan-name color-blue rounded-5 d-inline-block">{{planName}}</div>
+                <div class="price">
+                    <p class="mb-0">{{price}}</p>
+                    <span>{{planTime}}</span>
+                </div>
+                <p class="line-3">{{textInfo}}</p>
+                <hr>
+                <ul>
+                    <li v-for="points in services" class="d-flex gap-1">
+                        <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue"/>
+                        <p>{{points}}</p>
+                    </li>
+                </ul>
+                <a href="#" class="btn btn-primary w-100">{{btnText}}</a>
+                <p class="text-cc">{{textCc}}</p>
+    
             </div>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <br>
-            <ul>
-                <li class="d-flex gap-1" v-for="(item,i) in servicesList" :key="i">
-                    <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue"/>
-                    <p>{{item}}</p>
-                </li>
-            </ul>
-            <a href="#" class="btn btn-primary w-100">Buy now</a>
-            <p class="text-cc">*No credit card required</p>
 
         </div>
-    </div>
+    <!-- </div> -->
 </template>
 <script>
+// import { stringifyExpression } from '@vue/compiler-core';
+
 
 export default {
     name: 'PriceCard',
+    props: {
+        planName: String,
+        price: String,
+        planTime: String,
+        textInfo: String,
+        services: Array,
+        btnText: String,
+        textCc: String,
+    },
     data() {
         return {
-            servicesList: ['full access', 'enhanced security', 'source files', '1 domain free', 'enhanced security']
+            // servicesList: ['full access', 'enhanced security', 'source files', '1 domain free', 'enhanced security']
 
         }
     },
@@ -41,29 +55,46 @@ export default {
 .plan-card {
     border: 1px solid black;
     padding: 17px;
+    color: $grey-price-card;
 }
 
-.sticker {
+.line-3 {
+    height: 4rem;
+}
+
+.plan-name {
     padding: .3rem 1rem;
-    background-color: $bg-sticker;
-    border: 1px solid $border-sticker !important;
+    background-color: $bg-plan-name;
+    border: 1px solid $border-plan-name !important;
 }
 
 .price {
-    font-size: 2rem;
     font-weight: 600;
+
+    p {
+        font-size: 2rem;
+        color: $price-color;
+    }
+
+    br {
+        line-height: 10px;
+    }
+
+    span {
+        font-size: .7rem;
+    }
 }
 
 li {
     text-transform: capitalize;
 
-    .color-blue {
-        color: $blu !important;
-    }
-
     p {
         line-height: 16px;
     }
+}
+
+.color-blue {
+    color: $blu !important;
 }
 
 .text-cc {

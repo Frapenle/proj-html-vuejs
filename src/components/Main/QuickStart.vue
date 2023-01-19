65a2a75bdbbeaa59aac2c494c244a21b9bb64d44
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 11:02:56 +0100

    Add quickstart, import pricingPlans

--- src/components/Main/QuickStart.vue
+++ src/components/Main/QuickStart.vue
@@ -1,10 +1,11 @@
 <script >
-export default {
-    name: 'QuickStart',
-    data() {
-        return {
+import PricingPlans from './PricingPlans.vue';
 
-        }
+export default {
+    name: "QuickStart",
+    components: { PricingPlans },
+    data() {
+        return {};
     },
 }
 
@@ -12,11 +13,26 @@ export default {
 </script>
 
 <template>
-    <section id="quick-start">QuickStart</section>
+    <section id="quick-start">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-12">
+                    <h2>Awesome isn't it? Let's dive in!</h2>
+                    <p>Start working with <span>Landrick</span> that can provide everything you need to generate<br>
+                        awareness, drive traffic,connect.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <PricingPlans />
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
-@use '../src/styles/general.scss' as *;
-@use '../src/styles/partials/variables' as *;
+@use '../../styles/general.scss' as *;
+@use '../../styles/partials/variables' as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
 </style>

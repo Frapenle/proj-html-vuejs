7d3a2e602e2cac20a53ee8d6096d2ebd7fb89730
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 17:28:26 +0100

    Add footerbottom

---
+++ src/components/Footer/FooterBottom.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <h2>FooterBottom</h2>
+</template>
+<script>
+export default {
+    name: FooterBottom,
+    data() {
+        return {
+
+        }
+    },
+
+}
+</script>
+<style lang="scss">
+@use '../../styles/general.scss' as *;
+@use '../../styles/partials/variables' as *;
+@use "bootstrap/scss/bootstrap.scss" as *;
+</style>

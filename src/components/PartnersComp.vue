fdcd053dd748b757366583488b242966ff7c3155
francescotrudu <francescotrudu84@gmail.com>
2023-01-19 09:29:17 +0100

    Add partners list

--- src/components/PartnersComp.vue
+++ src/components/PartnersComp.vue
@@ -3,7 +3,13 @@ export default {
     name: 'PartnersComp',
     data() {
         return {
+            partners: ['amazon', 'google', 'lenovo', 'paypal', 'shopify', 'spotify']
 
+        }
+    },
+    methods: {
+        getImagePath: function (img) {
+            return new URL(`../assets/img/${img}.svg`, import.meta.url).href;
         }
     },
 }
@@ -11,7 +17,20 @@ export default {
 </script>
 
 <template>
-    <section id="partners">Partners</section>
+    <section id="partners">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="partners-wrapper d-flex">
+                        <div :id="partner" v-for="(partner, i) in partners" :key="i">
+                            <img :src="getImagePath(partner)" :alt="partner + ' logo'" width="80">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
 
 </template>
 
@@ -19,4 +38,9 @@ export default {
 @use '../styles/general.scss' as *;
 @use '../styles/partials/variables' as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+.partners-wrapper {
+    gap: 3rem;
+    justify-content: center;
+}
 </style>

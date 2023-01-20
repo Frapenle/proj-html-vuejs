50ad6c33df1a5bfe9ada04fd5eaf09167122678a
francescotrudu <francescotrudu84@gmail.com>
2023-01-20 16:05:52 +0100

    Add featuresList and

--- src/components/Main/KeyFeature/KeyFeatures.vue
+++ src/components/Main/KeyFeature/KeyFeatures.vue
@@ -8,12 +8,37 @@ export default {
 
     data() {
         return {
+            featuresList: [
+                {
+                    icon: 'fa-solid fa-display',
+                    title: 'Modular',
+                    text: 'Composed in a pseudo-Latin language which more or less corresponds.',
+                    readMore: 'All the basics for businessthat are just getting started.',
+                },
+                {
+                    icon: 'fa-solid fa-display',
+                    title: 'Responsive',
+                    text: 'Composed in a pseudo-Latin language which more or less corresponds.',
+                    readMore: 'All the basics for businessthat are just getting started.',
+                },
+                {
+                    icon: 'fa-solid fa-display',
+                    title: 'Customizable',
+                    text: 'Composed in a pseudo-Latin language which more or less corresponds.',
+                    readMore: 'All the basics for businessthat are just getting started.',
+                },
+                {
+                    icon: 'fa-solid fa-display',
+                    title: 'Scalable',
+                    text: 'Composed in a pseudo-Latin language which more or less corresponds.',
+                    readMore: 'All the basics for businessthat are just getting started.',
+                },
+            ],
 
         }
     },
 }
 
-//import
 </script>
 
 <template>
@@ -22,18 +47,15 @@ export default {
             <div class="row">
                 <div class="col-12">
                     <h2>Key Features</h2>
-                    <p>Start working with <span>Landrick</span> that can provide everything you need to generate<br>
+                    <p class="py-3 mb-5">Start working with <span>Landrick</span> that can provide everything you need
+                        to
+                        generate<br>
                         awareness, drive traffic,connect.</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-3">
-                    <div class="cards">
-                        <!-- insert 4 Features card -->
-                        <FeaturesCard />
-
-                    </div>
-                </div>
+            <div class="row justify-content-between">
+                <FeaturesCard v-for="(feature, i) in featuresList" :key="i" :icon="feature.icon" :title="feature.title"
+                    :text="feature.text" :readMore="feature.readMore" />
             </div>
         </div>
     </section>
@@ -43,4 +65,8 @@ export default {
 @use '../../../styles/partials/variables' as *;
 @use '../../../styles/general.scss' as *;
 @use "bootstrap/scss/bootstrap.scss" as *;
+
+#key-features {
+    padding: 70px 0;
+}
 </style>

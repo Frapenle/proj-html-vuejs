da4803d178acbd186770d60181861ab656e00d75
francescotrudu <francescotrudu84@gmail.com>
2023-01-20 17:15:25 +0100

    Add margin

--- src/components/Main/BuildTools.vue
+++ src/components/Main/BuildTools.vue
@@ -18,21 +18,21 @@ export default {
                     <h1>Build tools and<br>extensive documentation</h1>
                     <p class="py-3">You can combine all the Landrick templates into a single one, you can take a
                         component from the Application theme and use it in the Website.</p>
-                    <ul>
+                    <ul class="mb-4">
                         <li class="d-flex gap-1">
-                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue fs-1" />
+                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue font-1" />
                             <p>Digital Marketing Solution for Tomorrow</p>
                         </li>
                         <li class="d-flex gap-1">
-                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue fs-1" />
+                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue font-1" />
                             <p>Our Talented & Experienced Marketing Agency</p>
                         </li>
                         <li class="d-flex gap-1">
-                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue fs-1" />
+                            <font-awesome-icon icon="fa-regular fa-circle-check" class="color-blue font-1" />
                             <p>Create your own skin to match your brand</p>
                         </li>
                     </ul>
-                    <button class="btn btn-primary">Read more &nbsp;<font-awesome-icon icon="fa-solid fa-arrow-right"
+                    <button class="btn btn-blu">Read more &nbsp;<font-awesome-icon icon="fa-solid fa-arrow-right"
                             class="center" /></button>
 
                 </div>
@@ -56,19 +56,25 @@ export default {
     padding: 70px 0;
 }
 
+li {
+    align-items: center;
+    margin-top: 1rem;
+}
+
 li p {
     font-size: .9rem;
+    margin: 0;
 }
 
 .center {
-    vertical-align: text-bottom;
+    vertical-align: middle;
 }
 
 .color-blue {
     color: $blu;
 }
 
-.fs-1 {
-    font-size: .4rem;
+.font-1 {
+    font-size: 1rem;
 }
 </style>
